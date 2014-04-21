6d566b8b4960c510040e1c5ea3c8e50f3bba296a
bromine0x23 <bromine0x23@gmail.com>
2014-04-21 21:58:43 +0800

    EVE插件添加了物品、星系、空间站信息查询功能

--- plugins/pluginEVE.rb
+++ plugins/pluginEVE.rb
@@ -14,67 +14,187 @@ require 'rexml/document'
 class PluginEVE < PluginNicknameResponserBase
 	NAME = 'EVE插件'
 	AUTHOR = 'BR'
-	VERSION = '1.7'
+	VERSION = '1.8'
 	DESCRIPTION = '我们的征途的星辰大海'
 	MANUAL = <<MANUAL.strip
 == 吉他价格查询 ==
-EVE 市场 <物品>（用逗号(,，)分割）
+EVE 市场 <物品>
 EVE 基础矿物
+EVE 物品 <物品>
+EVE 空间站 <星系>
 MANUAL
 	PRIORITY = 0
-	
-	COMMAND_PATTERN = /^EVE\s*(?<command>.+)/i
-	MARKET_PATTERN  = /^市场\s*(?<item_names>.+)/
-	MINERAL_PATTERN = /^基础矿物$/i
+
+	CALL_PATTERN = /^EVE\s*(?<command>.+)/i
+	COMMAND_MINERAL      = /^基础矿物$/
+	COMMAND_ITEM_INFO    = /^物品\s*(?<item_name>.+)$/
+	COMMAND_STATION_INFO = /^空间站\s*(?<system_name>.+)$/
+	COMMAND_SYSTEM_INFO  = /^星系\s*(?<system_name>.+)$/
+	COMMAND_MARKET       = /^市场\s*(?<item_name>.+)/
 
 	URI_MINERAL = 'http://www.ceve-market.org/api/evemon'
 
-	JSON_KEY_BUY, JSON_KEY_SELL  = 'buy', 'sell'
+	JSON_KEY_BUY, JSON_KEY_SELL = 'buy', 'sell'
 	JSON_KEY_MAX, JSON_KEY_MIN  = 'max', 'min'
-
-	NO_PRICE = '暂无出价'
 
 	XPATH_MINERAL = 'minerals/mineral'
 
-	PATTERN_ITEM_NAMES_SEPARATOR = /,|，/
 	PATTERN_THOUSAND_SEPARATOR = /(?<=\d)(?=(\d\d\d)+\.)/
-	THOUSAND_SEPARATOR = ','
 
-	def format_price(price)
-		price.gsub(PATTERN_THOUSAND_SEPARATOR, THOUSAND_SEPARATOR)
+	DB_FILE = "#{PLUGIN_DIRECTORY}/pluginEVE.db"
+
+	SQL_SELECT_ITEM_ID = <<SQL
+SELECT id FROM items WHERE name = ?
+SQL
+
+	SQL_SELECT_ITEM = <<SQL
+SELECT id, description, volume, mass, marketgroup_id FROM items WHERE name = ?
+SQL
+
+	SQL_SELECT_MARKGROUP = <<SQL
+SELECT name, parent_id FROM marketgroups WHERE id = ?
+SQL
+
+	SQL_SELECT_ITEM_ATTRITUBES = <<SQL
+SELECT attributes.name, item_attributes.value, units.name
+FROM item_attributes, attributes, units
+WHERE item_attributes.item_id = ? AND item_attributes.attribute_id = attributes.id AND attributes.unit_id = units.id
+SQL
+
+	SQL_SELECT_STATION_NAMES = <<SQL
+SELECT name FROM stations WHERE system_id = (SELECT id FROM systems WHERE name = ?)
+SQL
+
+	SQL_SELECT_SYSTEM = <<SQL
+SELECT id, security FROM systems WHERE name = ?
+SQL
+
+	SQL_SELECT_NEAR_SYSTEMS = <<SQL
+SELECT name, security FROM systems WHERE id IN (SELECT to_id FROM system_jumps WHERE from_id = ?)
+SQL
+
+	def on_load
+		super
+		@db = SQLite3::Database.open DB_FILE
 	end
 
-	def get_response(uin, sender_qq, sender_nickname, message, time)
+	def on_unload
+		super
+		@db.close
+	end
+
+	def format_price(price)
+		price.round(2).to_s.gsub(PATTERN_THOUSAND_SEPARATOR, @thousand_separator)
+	end
+
+	def get_response(uin, sender_qq, sender_nickname, command, time)
 		# super # FOR DEBUG
-		if COMMAND_PATTERN =~ message
+		if CALL_PATTERN =~ command
 			command = $~[:command]
-			if MINERAL_PATTERN =~ command
-				response = self.response_header_with_nickname(sender_nickname)
-				REXML::Document.new(Net::HTTP.get(URI(URI_MINERAL))).each_element(XPATH_MINERAL) do |element|
-					response << <<RESPONSE
-#{element[0].text}：#{format_price(element[1].text)} ISK
+			function_mineral(     uin, sender_qq, sender_nickname, command, time) ||
+			function_item_info(   uin, sender_qq, sender_nickname, command, time) ||
+			function_station_info(uin, sender_qq, sender_nickname, command, time) ||
+			function_market(      uin, sender_qq, sender_nickname, command, time) ||
+			function_system_info( uin, sender_qq, sender_nickname, command, time)
+		end
+	end
+
+	def function_mineral(_, _, _, command, _)
+		if COMMAND_MINERAL =~ command
+			response = ''
+			REXML::Document.new(Net::HTTP.get(URI(URI_MINERAL))).each_element(XPATH_MINERAL) do |element|
+				response << <<RESPONSE
+#{element[0].text}：#{format_price(element[1].text)} #{@units[:price]}
 RESPONSE
-				end
-				response
-			elsif MARKET_PATTERN =~ command 
-				response = response_header_with_nickname sender_nickname
-				$~[:item_names].split(PATTERN_ITEM_NAMES_SEPARATOR).each do |item_name|
-					if @datas.has_key? item_name
-						json_data = JSON.parse(Net::HTTP.get(URI("http://www.ceve-market.org/api/market/region/10000002/system/30000142/type/#{@datas[item_name]}.json")))
-						buy  = json_data[JSON_KEY_BUY][JSON_KEY_MAX]
-						sell = json_data[JSON_KEY_SELL][JSON_KEY_MIN]
-						response << <<RESPONSE
+			end
+			response
+		end
+	end
+
+	def function_market(_, _, _, command, _)
+		if COMMAND_MARKET =~ command
+			item_name = $~[:item_name]
+			item_id = @db.get_first_value(SQL_SELECT_ITEM_ID, item_name)
+			if item_id
+				json_data = JSON.parse(Net::HTTP.get(URI("http://www.ceve-market.org/api/market/region/10000002/system/30000142/type/#{item_id}.json")))
+				buy  = json_data[JSON_KEY_BUY][JSON_KEY_MAX]
+				sell = json_data[JSON_KEY_SELL][JSON_KEY_MIN]
+				<<RESPONSE
 #{item_name} 吉他报价
-求购：#{buy  ? format_price( buy.round(2).to_s) : NO_PRICE} ISK
-出售：#{sell ? format_price(sell.round(2).to_s) : NO_PRICE} ISK
+求购：#{buy  ? format_price( buy) : @responses[:no_price]} #{@units[:price]}
+出售：#{sell ? format_price(sell) : @responses[:no_price]} #{@units[:price]}
 RESPONSE
-					else
-						response << <<RESPONSE
-不存在物品：#{item_name}
-RESPONSE
+			else
+				@responses[:no_item] % item_name
+			end
+		end
+	end
+
+	def function_item_info(_, _, _, command, _)
+		if COMMAND_ITEM_INFO =~ command
+			item_name = $~[:item_name]
+			result = @db.get_first_row(SQL_SELECT_ITEM, item_name)
+			if result
+				item_id        = result[0]
+				description    = result[1]
+				volume         = result[2]
+				mass           = result[3]
+				marketgroup_id = result[4]
+				marketgroup_names = []
+				while marketgroup_id
+					@db.execute(SQL_SELECT_MARKGROUP, marketgroup_id) do |row|
+						marketgroup_names << row[0]
+						marketgroup_id = row[1]
 					end
 				end
-				response
+				item_attributes = []
+				@db.execute(SQL_SELECT_ITEM_ATTRITUBES, item_id) do |row|
+					item_attributes << "#{row[0]}：#{row[1]} #{row[2]}"
+				end
+				<<RESPONSE
+#{item_name}
+市场分类：#{marketgroup_names.reverse!.join(' - ')}
+体积：#{volume} ㎥　质量：#{mass} ㎏
+描述：#{description}
+#{item_attributes.join("\n")}
+RESPONSE
+			else
+				@responses[:no_item] % item_name
+			end
+		end
+	end
+
+	def function_station_info(_, _, _, command, _)
+		if COMMAND_STATION_INFO =~ command
+			system_name = $~[:system_name]
+			result = @db.execute(SQL_SELECT_STATION_NAMES, system_name)
+			if result
+				result.map!{|row| row[0]}.join("\n")
+			else
+				@responses[:no_system] % system_name
+			end
+		end
+	end
+
+	def function_system_info(_, _, _, command, _)
+		if COMMAND_SYSTEM_INFO =~ command
+			system_name = $~[:system_name]
+			result = @db.get_first_row(SQL_SELECT_SYSTEM, system_name)
+			if result
+				system_id = result[0]
+				security  = result[1]
+				near_systems = ''
+				@db.execute(SQL_SELECT_NEAR_SYSTEMS, system_id) do |row|
+					near_systems << "#{row[0]} #{row[1]}\n"
+				end
+				<<RESPONSE
+#{system_name}
+安等：#{security}
+相邻星系：
+#{near_systems.empty? ? '无' : near_systems}
+RESPONSE
+			else
+				@responses[:no_system] % system_name
 			end
 		end
 	end
